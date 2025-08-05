c50889227c5b00b12249900cc7188e486bdc6852
priyanshibaria16 <priyanshibaria05@gmail.com>
2025-08-06 00:59:25 +0530

    Title

--- Edusity/src/App.jsx
+++ Edusity/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'; 
 import Programs from './components/Programs/Programs';
+import Title from './components/Title/Title';
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
       <Navbar/>
       <Hero />
       <div className="container">
+          <Title />
           <Programs />
       </div>
       
