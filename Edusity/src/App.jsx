8665ea52923433d27df15a3adc7bc15f5d017fec
priyanshibaria16 <priyanshibaria05@gmail.com>
2025-08-06 00:27:16 +0530

    Programs

--- Edusity/src/App.jsx
+++ Edusity/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'; 
+import Programs from './components/Programs/Programs';
 
 export const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero />
+      <div className="container">
+          <Programs />
+      </div>
+      
     </div>
   )
 }
