0f17b448da20f60f540e6c0c187a4c2de595cb90
priyanshibaria16 <priyanshibaria05@gmail.com>
2025-08-05 23:01:00 +0530

    landing page

--- Edusity/src/App.jsx
+++ Edusity/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'; 
 
 export const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero />
     </div>
   )
 }
