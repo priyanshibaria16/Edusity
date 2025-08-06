c19700705c76d9966c83d0a1c6cc0746c1924829
priyanshibaria16 <priyanshibaria05@gmail.com>
2025-08-06 09:48:48 +0530

    About Page

--- Edusity/src/App.jsx
+++ Edusity/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'; 
 import Programs from './components/Programs/Programs';
 import Title from './components/Title/Title';
+import About from './components/About/About';
 
 export const App = () => {
   return (
@@ -12,6 +13,7 @@ export const App = () => {
       <div className="container">
           <Title />
           <Programs />
+          <About />
       </div>
       
     </div>
--- Edusity/src/components/About/About.jsx
+++ Edusity/src/components/About/About.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
 
 const About = () => {
   return (
     <div className='about'>
-    
+        <div className="about-left">
+            <img src={about_img} alt="" className='about-img' />
+            <img src={play_icon} alt="" className='play-icon' />
+        </div>
+        <div className="about-right">
+            <h3>ABOUT UNIVERSITY</h3>
+            <h2>Nurturing Tommorrow's Leaders Today</h2>
+            <p>At our university, learning goes beyond textbooks — it’s a journey of curiosity, collaboration, and purpose. We believe education should spark innovation, nurture creativity, and build character. That’s why our classrooms are not just spaces for lectures, but launchpads for ideas that shape the future.</p>
+            <p>Rooted in tradition yet driven by progress, we blend academic excellence with real-world exposure. From cutting-edge research labs to cultural festivals, entrepreneurial hubs to global partnerships — our students don’t just earn degrees, they craft stories worth telling. Here, every ambition finds direction, and every voice has the power to lead.</p>
+        </div>
     </div>
   )
 }
